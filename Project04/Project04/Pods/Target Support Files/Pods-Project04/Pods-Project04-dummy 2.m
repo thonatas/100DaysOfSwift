ed0d3ed01a2ab97132b2a05733f663353430593b
Thonatas Reis Borges <thonatas_borges@outlook.com>
2021-07-10 18:27:02 -0300

    [REMOVE] duplicate files

--- Project04/Project04/Pods/Target Support Files/Pods-Project04/Pods-Project04-dummy 2.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Project04 : NSObject
-@end
-@implementation PodsDummy_Pods_Project04
-@end
--- Project06/Project06/Pods/Target Support Files/Pods-Project06/Pods-Project06-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Project06 : NSObject
-@end
-@implementation PodsDummy_Pods_Project06
-@end
