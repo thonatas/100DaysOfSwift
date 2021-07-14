5b7530311c551672982abf59e4d5d2cd0b6f072a
Thonatas Reis Borges <thonatas_borges@outlook.com>
2021-07-13 21:39:37 -0300

    [CHANGE] Code indentation adjustments [CHANGE] from UICollectionViewController to UIVIewController + CollectionView Delegates

--- Project10/Project10/UIComponents/CollectionViewCell/PersonCollectionViewCell.swift
+++ Project10/Project10/UIComponents/CollectionViewCell/PersonCollectionViewCell.swift
@@ -9,59 +9,53 @@ import UIKit
 import SnapKit
 
 class PersonCollectionViewCell: UICollectionViewCell {
- 
-    //MARK: - DECLARATION OF VARIABLES / INITIALIZATION
-        static let identifier = "PersonCollectionViewCell"
-        
-        let imageView = UIImageView()
-        let nameLabel = UILabel()
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            //contentView.frame = CGRect(x: 0, y: 0, width: 140, height: 180)
-            //contentView.clipsToBounds = true
-            backgroundColor = .white
-            layer.cornerRadius = 7
-            addImageView()
-            addNameLabel()
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-     
+    
+    static let identifier = "PersonCollectionViewCell"
+    
+    let imageView = UIImageView()
+    let nameLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        layer.cornerRadius = 7
+        addImageView()
+        addNameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     //MARK: - FUNCTIONS
-        private func addImageView() {
-            contentView.addSubview(imageView)
-            
-            imageView.contentMode = .scaleToFill
-            imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
-            imageView.layer.borderWidth = 2
-            imageView.layer.cornerRadius = 3
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            
-            imageView.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(10)
-                make.leading.equalToSuperview().offset(10)
-                make.trailing.equalToSuperview().offset(-10)
-                make.size.equalTo(120)
-            }
-        }
+    private func addImageView() {
+        contentView.addSubview(imageView)
         
-        private func addNameLabel() {
-            contentView.addSubview(nameLabel)
-            
-            nameLabel.font = .systemFont(ofSize: 15, weight: .regular)
-            nameLabel.textAlignment = .center
-            nameLabel.numberOfLines = 2
-            nameLabel.translatesAutoresizingMaskIntoConstraints = false
-
-            nameLabel.snp.makeConstraints { make in
-                make.top.equalTo(imageView.snp.bottom).offset(14)
-                make.leading.equalToSuperview().offset(10)
-                make.trailing.equalToSuperview().offset(-10)
-                make.height.equalTo(40)
-                make.width.equalTo(120)
-            }
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        imageView.layer.borderWidth = 2
+        imageView.layer.cornerRadius = 3
+        imageView.clipsToBounds = true
+        
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            make.size.equalTo(120)
         }
+    }
+    
+    private func addNameLabel() {
+        contentView.addSubview(nameLabel)
+        
+        nameLabel.font = .systemFont(ofSize: 15, weight: .regular)
+        nameLabel.textAlignment = .center
+        nameLabel.numberOfLines = 2
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(14)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+        }
+    }
 }
--- Project10/Project10/ViewControllers/PersonViewController.swift
+++ Project10/Project10/ViewControllers/PersonViewController.swift
@@ -8,28 +8,31 @@
 import UIKit
 import SnapKit
 
-class PersonViewController: UICollectionViewController {
+class PersonViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(PersonCollectionViewCell.self, forCellWithReuseIdentifier: PersonCollectionViewCell.identifier)
+        return collectionView
+    }()
 
     var people: [Person] = []
     
     // MARK: - Life Cycle View
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
-        self.collectionView.backgroundColor = .white
-        self.collectionView.register(PersonCollectionViewCell.self, forCellWithReuseIdentifier: PersonCollectionViewCell.identifier)
+        addCollectionView()
     }
     
-    // MARK: - Initializers
-    init() {
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionHeadersPinToVisibleBounds = true
-        super.init(collectionViewLayout: layout)
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
     }
     
     // MARK: - Functions
@@ -43,37 +46,17 @@ class PersonViewController: UICollectionViewController {
 }
 
 // MARK: - CollectionView functions
-extension PersonViewController {
-//    private func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize.init(width: view.frame.width, height: 180)
-//    }
+extension PersonViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            let size = (self.view.frame.width/2) - 16
-            return CGSize(width: size, height: size)
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-            return 16.0
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-            return 8.0
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-            return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-            return CGSize(width: collectionView.frame.width, height: 40)
-        }
-//
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return people.count
     }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PersonCollectionViewCell.identifier, for: indexPath) as? PersonCollectionViewCell else {
             fatalError("Unable to dequeue PersonCollectionViewCell.")
         }
@@ -82,14 +65,13 @@ extension PersonViewController {
         let path = getDocumentsDirectory().appendingPathComponent(person.image)
         
         cell.nameLabel.text = person.name
-        cell.imageView.translatesAutoresizingMaskIntoConstraints = false
         cell.imageView.image = UIImage(contentsOfFile: path.path)
         cell.imageView.contentMode = .scaleAspectFill
 
         return cell
     }
     
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
 
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
@@ -106,8 +88,26 @@ extension PersonViewController {
 
         present(ac, animated: true)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.width/2) - 24
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 12, bottom: 0, right: 12)
+    }
 }
 
+// MARK: - UIImagePicker & UINavigationController Delegates
 extension PersonViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
@@ -129,5 +129,18 @@ extension PersonViewController: UIImagePickerControllerDelegate, UINavigationCon
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
+    }
+}
+
+//MARK: - Constraints
+extension PersonViewController {
+    
+    private func addCollectionView() {
+        self.view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self.view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+        }
     }
 }
