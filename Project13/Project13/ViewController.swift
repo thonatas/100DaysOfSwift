07b9dac0df8cbb4bec0ccb646160a4bc62dfae5d
Thonatas Reis Borges <thonatas_borges@outlook.com>
2021-07-23 21:13:38 -0300

    [ADD] SnapKit SPM

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
     private func setupView() {
